f731c860b4c424f4c2fe280300780c3ae8b0c0d5
OlgaMartazova <olyamarta34@gmail.com>
2021-04-14 15:58:51 +0300

    modified

--- semestrovka1/time.cpp
+++ semestrovka1/time.cpp
@@ -6,14 +6,20 @@ using namespace std::chrono;
 
 int main() {
     ifstream in;
+    //file with 100 sets of data
     in.open(".\\input.txt");
+    ofstream res;
+    //file with two columns: number of elements in each set of data
+    //and average execution time
+    res.open(".\\1.txt");
     for (int t = 0; t < 100; t ++) {
         int x;
         in >> x;
         ofstream out;
+        //file with 1 set of data (made from input.txt)
         out.open(".\\output.txt");
         out << x << endl;
-        cout << x << "\t";
+        res << x << "\t";
         for (int i = 0; i < x; i++) {
             int y;
             in >> y;
@@ -27,16 +33,15 @@ int main() {
         double count = 0;
         for (int l = 0; l < 10; l ++) {
             auto start = high_resolution_clock::now();
-            //вызов командной строки, ожидание пока программа закончится
             system(".\\code_.exe < .\\output.txt");
             auto stop = high_resolution_clock::now();
             auto duration = duration_cast<microseconds>(stop - start);
             count += duration.count();
         }
         count = (int) count / 10;
-        cout << count << endl;
+        res << count << endl;
     }
     in.close();
+    res.close();
     return 0;
 }
-//C:\Users\Olga\CLionProjects\aisd\semestrovka>..\cmake-build-debug\aisd.exe > 1.txt
